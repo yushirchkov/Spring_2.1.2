ac4c5f878cf60644dc7960eeb5b331f72a00ccfa
Юрок <yushirchkov@gmail.com>
2024-03-10 21:29:27 +0300

    Добавил реализацию

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+        System.out.println(bean);
+        System.out.println(bean2);
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat bean2Cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+        System.out.println(beanCat == bean2Cat);
+        System.out.println(beanCat);
+        System.out.println(bean2Cat);
     }
 }
--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,15 +1,14 @@
-public class HelloWorld {
- 
+public class Cat {
     private String message;
- 
+
     public String getMessage() {
 
         return message;
     }
- 
+
     public void setMessage(String message) {
 
         this.message = message;
     }
-     
 }
+
